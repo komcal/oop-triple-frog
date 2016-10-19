673860e450656625f6d316e55a57c93914bf1fe8
komcal <kom.sujautra@gmail.com>
2016-10-19 18:58:23 +0700

    create player to game screen

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -1,16 +1,23 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GameScreen extends ScreenAdapter {
 	private TripleFrog tripleFrog;
+	private Texture playerImg;
     
 	public GameScreen(TripleFrog tripleFrog) {
         this.tripleFrog = tripleFrog;
+        playerImg = new Texture("player.png");
     }
     @Override
     public void render(float delta) {
-        System.out.println("Hello " + delta);
+        SpriteBatch batch = tripleFrog.batch;
+        batch.begin();
+        batch.draw(playerImg, 100, 100);
+        batch.end();
     }
 
 }
