4e23a375ffb7efa4868363a52ac7e0a7a1ead1f1
komcal <kom.sujautra@gmail.com>
2016-10-19 21:38:16 +0700

    simple move player

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -1,23 +1,47 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GameScreen extends ScreenAdapter {
 	private TripleFrog tripleFrog;
 	private Texture playerImg;
+    private int x;
+    private int y;
     
 	public GameScreen(TripleFrog tripleFrog) {
         this.tripleFrog = tripleFrog;
         playerImg = new Texture("player.png");
+        x = 100;
+        y = 100;
     }
     @Override
     public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    	update(delta);
         SpriteBatch batch = tripleFrog.batch;
         batch.begin();
-        batch.draw(playerImg, 100, 100);
+        batch.draw(playerImg, x, y);
         batch.end();
+    }
+    private void update(float delta) {
+        if(Gdx.input.isKeyPressed(Keys.LEFT)) {
+            x -= 10;
+        }
+        if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
+            x += 10;
+        }
+        if(Gdx.input.isKeyPressed(Keys.UP)) {
+            y -= 10;
+        }
+        if(Gdx.input.isKeyPressed(Keys.DOWN)) {
+            y += 10;
+        }
     }
 
 }
