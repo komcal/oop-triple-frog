eb25ae574458db268e1fabc7a11cc7dbe66f915f
komcal <kom.sujautra@gmail.com>
2016-11-01 15:19:54 +0700

    tranform item when update

--- core/src/com/mygdx/game/Board.java
+++ core/src/com/mygdx/game/Board.java
@@ -16,6 +16,7 @@ public class Board {
     };
     private int height;
     private int width;
+    private int countStickItem;
  
     public Board() {
         height = MAP.length;
@@ -61,17 +62,31 @@ public class Board {
     public void update(int r, int c, char item) {
     	if (isEmptyArea(r, c)) {
     		updateNextToItem(r, c, item);
+    		if(countStickItem >= 3) {
+    			item = tranformItem(item);
+    		}
     		MAP[r] = MAP[r].substring(0, c) + item + MAP[r].substring(c + 1);
     	}
     	else if (item == '.') {
     		MAP[r] = MAP[r].substring(0, c) + item + MAP[r].substring(c + 1);
     	}
     }
+    
+    private char tranformItem(char item) {
+    	if(item == '3') {
+    		item = '.';
+    	}
+    	else {
+    		item++;
+    	}
+    	return item;
+    }
+    
     public void updateNextToItem(int r, int c, char item) {
     	int[] queueX = new int[100000];
     	int[] queueY = new int[100000];
     	int q = 0;
-    	int count = 1;
+    	countStickItem = 1;
     	queueX[q] = c;
 		queueY[q] = r;
 		q++;
@@ -79,32 +94,32 @@ public class Board {
 			q--;
 			c = queueX[q];
 			r = queueY[q];
-			if(count >= 3) {
+			if(countStickItem >= 3) {
 				update(r, c, '.');
 			}
     		if (isSameItem(r-1, c, item)) {
 				queueX[q] = c;
 				queueY[q] = r-1;
 				q++;
-				count++;
+				countStickItem++;
 			}
 			if (isSameItem(r+1, c, item)) {
 				queueX[q] = c;
 				queueY[q] = r+1;
 				q++;
-				count++;
+				countStickItem++;
 			}
 			if (isSameItem(r, c-1, item)) {
 				queueX[q] = c-1;
 				queueY[q] = r;
 				q++;
-				count++;
+				countStickItem++;
 			}
 			if (isSameItem(r, c+1, item)) {
 				queueX[q] = c+1;
 				queueY[q] = r;
 				q++;
-				count++;
+				countStickItem++;
 			}
 			
 		}
